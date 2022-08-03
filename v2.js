66ef9a52ce62528f17e893d35e610cbb8d7f829a
muvipro <110364252+muvipro@users.noreply.github.com>
2022-08-03 13:59:24 +0800

    Create v2.js

---
+++ v2.js
@@ -0,0 +1,8 @@
+var dataUrl = [
+    'https://inquiryclank.com/pgy7h6cw?key=756a1bbb1d4d12b56cb50d97b90b7a51',
+    'https://scareshortnovel.com/k4qn8fta?key=53a079ee752630cb47b753228c9edbd6',
+    'https://www.highperformancegate.com/u843uwte?key=3e73b89a9ed9d2e16a5d193c60980d28',
+    'https://aliveperjuryruby.com/xehjv0hgr?key=c9955a9940bafccb93ff39894f51ad31'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
